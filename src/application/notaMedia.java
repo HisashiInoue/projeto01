45cc4647baae9e242c523e40e00ce72d7764f0d3
Hisashi Inoue <eng.hisashi@gmail.com>
2020-01-17 07:33:22 -0300

    Projeto criado

---
+++ src/application/notaMedia.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class notaMedia {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("Alô mundo!!");
+
+	}
+
+}
